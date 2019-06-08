5da6e62af05db1c4c2e8c56342107e206322de13
gauravjeetsingh <gaurav.ishwerdas@gmail.com>
2019-06-08 10:58:03 -0700

    adds prabhki and raaj font

--- src/js/constants.ts
+++ src/js/constants.ts
@@ -142,11 +142,13 @@ export const TEXTS = {
 export const FONT_OPTIONS = {
   anmol_lipi: 'Anmol Lipi',
   anmol_bold: 'Anmol Lipi Bold',
-  notosans: 'Noto Sans',
-  notosans_bold: 'Noto Sans Bold',
   gurmukhi_normal: 'Gurbani Akhar',
   gurmukhi_heavy: 'Gurbani Akhar Heavy',
   gurmukhi_thick: 'Gurbani Akhar Thick',
+  notosans: 'Noto Sans',
+  notosans_bold: 'Noto Sans Bold',
+  prabhki: 'Prabhki',
+  raaj: 'Raaj',
   unicode_font: 'Unicode',
 };
 
